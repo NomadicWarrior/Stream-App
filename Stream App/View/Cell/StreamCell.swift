3d45577300e50fbe34056f30dbf0d923ee3450b8
Nomadic <n.akylbekov@gmail.com>
2020-11-27 00:44:49 +0600

    Set up Collection view

---
+++ Stream App/View/Cell/StreamCell.swift
@@ -0,0 +1,24 @@
+//
+//  StreamCell.swift
+//  Stream App
+//
+//  Created by Nomadic on 11/27/20.
+//  Copyright © 2020 Nomadic. All rights reserved.
+//
+
+import UIKit
+
+class StreamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Stream App/View/Extension/UIViewController + Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController + Extension.swift
+//  Stream App
+//
+//  Created by Nomadic on 11/27/20.
+//  Copyright © 2020 Nomadic. All rights reserved.
+//
+
+import Foundation
